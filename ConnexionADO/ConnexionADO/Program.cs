b8a4c5aa6b50cfd18ce005abe272f398010b6ce3
CaroCap <carolinecap.event@gmail.com>
2021-10-29 10:06:19 +0200

    ADO

--- ConnexionADO/ConnexionADO/Program.cs
+++ ConnexionADO/ConnexionADO/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary1;
+using System;
 using System.Data.SqlClient;
 
 namespace ConnexionADO
@@ -43,6 +44,30 @@ namespace ConnexionADO
                 }
             }
 
+            // INSTANCIER UN OBJET STUDENT
+            Student etudiant1 = new Student()
+            {
+                first_name = "Caroline",
+                last_name = "Cap",
+                birth_date = new DateTime(1990,1,16),
+                login = "CapCar",
+                section_id = 1120,
+                year_result = 16,
+                course_id = "WAD21"
+            };
+
+            // INSÉRER L'OBJET DANS LA BASE DE DONNÉE ET AFFICHER ID
+            using (SqlConnection connection = new SqlConnection(CONNEXION_ADO))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = $"INSERT INTO student(first_name, last_name, birth_date, login, section_id, year_result, course_id) output inserted.student_id values ('{etudiant1.first_name}', '{etudiant1.last_name}', '{etudiant1.birth_date.ToString("yyyy-MM-dd")}', '{etudiant1.login}', {etudiant1.section_id}, {etudiant1.year_result}, '{etudiant1.course_id}')";
+
+                    connection.Open();
+                    etudiant1.student_id = (int)command.ExecuteScalar();
+                }
+            }
+            Console.WriteLine($"{etudiant1?.student_id} - {etudiant1.first_name} {etudiant1.last_name}");
             Console.ReadLine();
         }
     }
